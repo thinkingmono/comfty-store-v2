d805dad5bb5047259f8d5134a58b1e8c30c798b8
Alejandro Garcia <thinkingmono92@gmail.com>
2023-12-27 18:02:51 -0500

    Contact.js | Adding JSX structure for newsletter section in home page. Use formspree provider to handle form email submitions. Hero.js | Adding home page's main banner JSX structure. Services.js | Adding services module JSX structure. Map over services array to render each service card. HomePage.js | Adding Hero, FeaturedProducts, Services and Contact components in JSX return.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+    <div className="section-center">
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi fuga, eligendi tenetur consectetur mollitia a officiis! Enim recusandae fuga iste!</p>
+        <form action='https://formspree.io/f/xayrwrlo' method='POST' className="contact-form">
+          <input type="email" className='form-input' placeholder='enter email' name='email'/>
+          <button type="submit" className='submit-btn'>Subscribe</button>
+        </form>
+      </div>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
