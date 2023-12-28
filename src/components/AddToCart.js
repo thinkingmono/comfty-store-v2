1c770e5354e6c76cbf47026ee47069b1008b449a
Alejandro Garcia <thinkingmono92@gmail.com>
2023-12-28 16:56:45 -0500

    AddToCart.js | Destructure product from props. Destructure product id, stock and colors from product. mainColor state variable to store and handle selected color. amount state variable to store and handle product quantity selection. increase and decrease functions to handle product quantity increment and decrement. Colors and quantity modules selection in JSX return through AmountButtons component. Pass increase, decrease and amount as a components parameters. Adding add to cart button without handler. AmountButtons.js | Destructure increase, decrease and amount from component props. Return increase and decrease buttons with amount indicator. Stars.js | Destructure stars and reviews from component props. Build tempStars 5 positions star icons array based in ternary checks between stars rating vs number and index case depend. Return stars rating with reviews quantity. SingleProductPage.js | Passing stars and reviews as arguments into Stars component. Update add to cart conditional check passing product as a parameter into AddToCart component.

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,8 +5,47 @@ import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product;
+  // console.log(colors);
+  const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((prevAmount) => {
+      let tempAmount = prevAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount
+    })
+  }
+  const decrease = () => {
+    setAmount((prevAmount) => {
+      let tempAmount = prevAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount
+    })
+  }
+
+  return <Wrapper>
+    <div className="colors">
+      <span>colors: </span>
+      <div>
+        {colors.map((color, index) => {
+          return (
+            <button type="button" key={index} className={`${mainColor === color ? 'color-btn active' : 'color-btn'}`} style={{ background: color }} onClick={() => setMainColor(color)}>{mainColor === color ? <FaCheck /> : null}</button>
+          )
+        })}
+      </div>
+    </div>
+    <div className="btn-container">
+      <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+      <Link to='/cart' className='btn' >Add to cart</Link>
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
